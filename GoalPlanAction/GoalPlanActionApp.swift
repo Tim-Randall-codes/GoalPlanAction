17412f88deeed0ebf47da9b878460c9dcf7ba459
Tim Randall <trtimrandall@gmail.com>
2021-11-16 19:52:59 +1100

    Initial Commit

---
+++ GoalPlanAction/GoalPlanActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalPlanActionApp.swift
+//  GoalPlanAction
+//
+//  Created by Tim Randall on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GoalPlanActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
